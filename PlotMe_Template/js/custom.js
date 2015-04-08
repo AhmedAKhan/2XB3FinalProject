c4125a7abdf134e1cf579efc8bdc15f23d6d572d
AhmedAKhan <khanaahmed10@gmail.com>
2015-04-08 11:54:23 -0400

    cleaned the development branch

--- PlotMe_Template/js/custom.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Saim on 4/1/2015.
- */
-$(document).ready(function() {
-    $("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-    });
-});
